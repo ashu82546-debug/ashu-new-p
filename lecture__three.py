54cc5c8ef6e3b36fa5d44422b0b6ccf0496eaf91
ashu82546-debug <ashu82546@gmail.com>
2026-07-06 20:05:16 +0530

    Add files via upload

---
+++ lecture__three.py
@@ -0,0 +1,127 @@
+
+
+# marks = [94.4, 87.5, 95.2, 66.4, 45.1]
+
+# print(marks)
+# print(len(marks))
+# print(marks[0])
+# print(marks[1])
+
+# student = ["Karan", 94.4, 17, "Delhi"]
+# print(student[0])
+# student[0] = "arjun"
+# print(student)
+
+# marks  = [85, 94, 76, 63, 48]
+# print(marks[1:])
+
+# marks  = [85, 94, 76, 63, 48]
+# print(marks[-3:-1])
+
+
+# list = ["apple", "banana", "lichi"]
+# print(list.append(4))
+# print(list.sort(reverse=True))
+# print(list)
+
+# list = ['a', 'd', 'r', 'e', 'c', 'b', 'f']
+# list.reverse()
+# print(list)
+
+# list  = [85, 94, 76, 63, 48]
+# list.insert(1, 5)
+# print(list)
+
+# list = [2, 1, 3, 1]
+# list.pop(2)
+# print(list)
+
+# tup = (2, 1, 3, 1)
+# print(tup)
+
+# tup = (1,)
+# print(tup)
+# print(type(tup))
+
+# tup = (1, 2, 3, 4,)
+# print(tup[1:3])
+
+# tup = (1, 2, 3, 4)
+# print(tup.index(2))
+
+
+# tup = (1, 2, 3, 4, 2, 2, 6, 2)
+# print(tup.count(2))
+
+
+# movies = []
+
+# mov1 = input("enter 1st movie: ")
+# mov2 = input("enter 2nd movie: ")
+# mov3 = input("enter 3rd movie: ")
+
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+
+# print(movies)
+
+
+# movies = []
+
+# mov = input("enter 1st movie: ")
+# movies.append(mov)
+# mov = input("enter 2nd movie: ")
+# movies.append(mov)
+# mov = input("enter 3rd movie: ")
+# movies.append(mov)
+
+# print(movies)
+
+
+# movies = []
+# movies.append(input("enter 1st movies: "))
+# movies.append(input("enter 2nd movies: "))
+# movies.append(input("enter 3rd movies: "))
+
+# print(movies)
+
+# list1 = [1, 2, 1]
+# list2 = [1, 2, 3]
+
+# copy_list1 = list1.copy()
+# copy_list1.reverse()
+
+# if (copy_list1 == list1):
+#    print("palindrome")
+# else:
+#    print("NOT palindrome")
+
+
+# list1 = [1, 2, 3]
+
+# copy_list1 = list1.copy()
+# copy_list1.reverse()
+
+# if (copy_list1 == list1):
+#    print("palindrome")
+# else:
+#    print("NOT palindrome")
+
+# list1 = ["m," "a", "a", "m", "p"]
+
+# copy_list1 = list1.copy()
+# copy_list1.reverse()
+
+# if (copy_list1 == list1):
+#    print("palindrome")
+# else:
+#    print("NOt palindrome")    
+
+
+# grade = ("C", "D", "A", "A", "B", "B", "A")
+# print(grade.count("A"))
+
+# grade = ["C", "D", "A", "A", "B", "B", "A"]
+# grade.sort()
+# print(grade)
